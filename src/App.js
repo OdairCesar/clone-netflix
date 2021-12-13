efa57809cafd3877b9732a945d77638694cd6cae
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-12-13 16:56:13 -0300

    Implementando a API do the movie data base. E criando o ambiente de desenvolvimento

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react'
 import './App.css';
+import apiTMDB from './service/tmdb'
 
 function App() {
+  const [ movieList, setMovieList ] = useState([])
+
+  useEffect(() => {
+    const loadAll = async() =>{
+      let list = await apiTMDB.getHomeList()
+      setMovieList(list)
+    }
+
+    loadAll()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='page'>
+      Header
+      Destaque
+      As Listas
+      Rodape basico
     </div>
   );
 }
---
+++ src/service/tmdb.js
@@ -0,0 +1,63 @@
+const API_KEY = '411275984af8534cc39019e18679c7ab';
+const API_BASE = 'http://api.themoviedb.org/3'
+
+/**
+ * LISTAS Á SEREM PEGAS NA API
+ * 
+ * ->originais da netflix
+ * ->recomendados
+ * ->em alta
+ * ->ação
+ * ->comédia
+ * ->terror
+ * ->romance
+ * ->documentários
+ * 
+ */
+
+const basicFetch = async (endpoint) =>{
+    const req = await fetch(`${API_BASE}${endpoint}`)
+    const json = await req.json()
+    return json
+}
+
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
+export default{
+    getHomeList: async () => {
+        return [
+            {
+                slug: 'originals',
+                title: 'Originais do Netflix',
+                items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+            }, {
+                slug: 'trending',
+                title: 'Recomendados para Você',
+                items: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+            }, {
+                slug: 'toprated',
+                title: 'Em alta',
+                items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
+            }, {
+                slug: 'action',
+                title: 'Ação',
+                items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
+            }, {
+                slug: 'comedy',
+                title: 'Comédia',
+                items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
+            }, {
+                slug: 'horror',
+                title: 'Terror',
+                items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
+            }, {
+                slug: 'romance',
+                title: 'Romance',
+                items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
+            }, {
+                slug: 'documentary',
+                title: 'Documentários',
+                items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
+            }
+        ]
+    }
+}

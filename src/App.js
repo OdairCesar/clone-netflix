549edd115ee97092465b37e50358d3f86e816d2f
Odair Cesar Ferreira <odairferreira97@gmail.com>
2021-12-15 19:59:01 -0300

    Pegando as todas as informações de um filme especifico.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React, {useEffect, useState} from 'react'
-import './App.css';
+import './App.css'
 
 import apiTMDB from './service/tmdb'
 
 import MovieRow from './components/MovieRow'
+import FeatureMovie from './components/FeaturedMovie'
 
 function App() {
   const [ movieList, setMovieList ] = useState([])
+  const [ featuredData, setFeaturedData ] = useState(null)
 
   useEffect(() => {
+    //Pegando a listas das categorias
     const loadAll = async() =>{
-      let list = await apiTMDB.getHomeList()
+      const list = await apiTMDB.getHomeList()
       setMovieList(list)
+
+      //Pegando o filme que ficará no destaque. Ele será aletoria e será também um original da netflix
+      const originals = list.filter(item => item.slug === 'originals')
+      const randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+      const chosen = originals[0].items.results[randomChosen]
+    
+      //Como as infomações do filme é incompletas, é feito uma nova pesquisa, para ter todas as informações do filme
+      const chosenInfo = await apiTMDB.getMovieInfo(chosen.id, 'tv')
+      setFeaturedData(chosenInfo)
     }
 
     loadAll()
@@ -20,7 +32,7 @@ function App() {
   return (
     <div className='page'>
       Header
-      Destaque
+      {featuredData ? <FeatureMovie item={featuredData} /> : null}
       <section className='lists'>
         {movieList.map((item, key)=>(
           <MovieRow title={item.title} items={item.items} key={key}/>
